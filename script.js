ea755969614acebdca91f1d5dfe9bbb70ffd231a
Nicolas Leão <nicolasleao.dev@gmail.com>
2026-04-22 09:24:11 -0300

    feat: implementar menu responsivo e adicionar seção de notícias com grid

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburguer = document.getElementById("hamburguer");
+const menu = document.querySelector(".menu");
+
+hamburguer.addEventListener("click", () => {
+  // Alterna a exibição do menu
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+});
